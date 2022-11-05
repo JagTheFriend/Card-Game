d10cf5b3e55e4fe88cfa4324ae080b7f9c30a73f
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 21:46:22 +0200

    Store the player's names

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,18 @@
 import { Server } from 'socket.io';
 
+const tableIds: Record<string, string[]> = {};
+
 function handleSocketIo_(io: Server) {
   io.on('connect', socket => {
     socket.on('room-join', (tableId, playerName) => {
-      socket.join(tableId)
-      socket.to(tableId).emit("new-player", playerName)
-    })
+      socket.join(tableId);
+      if (tableId in tableIds) {
+        tableIds[tableId].push(playerName);
+      } else {
+        tableIds[tableId] = [playerName];
+      }
+      io.sockets.in(tableId).emit('all-players', tableIds[tableId]);
+    });
   });
 }
 
