eff9041410d1bf48286439125011db2b45e00fc1
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 21:46:58 +0200

    Updated file

--- src/public/script.js
+++ src/public/script.js
@@ -2,19 +2,19 @@ const socket = io('/');
 const players = document.getElementById('players');
 const activityChat = document.getElementById('activityChat');
 const table = document.getElementById('table');
-const playerName = "jag"
+const playerName = 'jag';
 
 function addElementIntoList(listReference, data) {
-  const node = document.createElement("li");
-  node.className = "list-group-item";
+  const node = document.createElement('li');
+  node.className = 'list-group-item';
   const data_ = document.createTextNode(data);
   node.appendChild(data_);
   listReference.appendChild(node);
-  listReference.appendChild(document.createElement("br"));
+  listReference.appendChild(document.createElement('br'));
 }
 
 socket.emit('room-join', TABLE_ID, playerName);
-addElementIntoList(players, playerName)
+addElementIntoList(players, playerName);
 
 socket.on('all-players', receivedPlayerNames => {
   console.log(receivedPlayerNames);
