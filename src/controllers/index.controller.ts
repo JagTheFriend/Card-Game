879232278315d12eb62da81564fd497769e1fedf
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 20:25:51 +0200

    Updated controller

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,11 +3,19 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
-      res.status(200).render("index");
+      res.sendStatus(200);
     } catch (error) {
       next(error);
     }
   };
+
+  public joinTable = (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      res.status(200).render("index", { tableId: req.params.tableId })
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 export default IndexController;
