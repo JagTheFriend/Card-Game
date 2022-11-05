49b1e477fc5e814d61fe6c60617b7cde800448bc
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 15:27:00 +0200

    Updated file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
 import swaggerJSDoc from 'swagger-jsdoc';
@@ -11,6 +11,7 @@ import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
 import { Routes } from '@interfaces/routes.interface';
 import errorMiddleware from '@middlewares/error.middleware';
 import { logger, stream } from '@utils/logger';
+import path from 'path';
 
 class App {
   public app: express.Application;
@@ -42,10 +43,14 @@ class App {
   }
 
   private initializeMiddlewares() {
+    this.app.set('views', path.join(__dirname, '/views'));
+    this.app.set('view engine', 'ejs');
+    this.app.use(express.static(path.join(__dirname, '/public')));
+
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
-    this.app.use(helmet());
+    // this.app.use(helmet());
     this.app.use(compression());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
