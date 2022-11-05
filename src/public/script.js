fe71051fb746b68001f50b8a44b16d4a85b8ad5a
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 21:08:37 +0200

    It is now able to add new players into the list

--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,21 @@
-const socket = io("/");
-const players = document.getElementById("players");
-const activityChat = document.getElementById("activityChat");
-const table = document.getElementById("table");
+const socket = io('/');
+const players = document.getElementById('players');
+const activityChat = document.getElementById('activityChat');
+const table = document.getElementById('table');
+const playerName = "jag"
 
-socket.emit("room-join", TABLE_ID, "Jag")
+function addElementIntoList(listReference, data) {
+  const node = document.createElement("li");
+  node.className = "list-group-item";
+  const data_ = document.createTextNode(data);
+  node.appendChild(data_);
+  listReference.appendChild(node);
+  listReference.appendChild(document.createElement("br"));
+}
+
+socket.emit('room-join', TABLE_ID, playerName);
+addElementIntoList(players, playerName)
+
+socket.on('new-player', receivedPlayerName => {
+  addElementIntoList(players, receivedPlayerName)
+});
