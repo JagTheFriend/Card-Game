d7f74621346ebd817b2daecbce52666d8476a1fa
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-05 20:29:59 +0200

    Get lists by ID

--- src/public/script.js
+++ src/public/script.js
@@ -1,3 +1,6 @@
 const socket = io("/");
+const players = document.getElementById("players");
+const activityChat = document.getElementById("activityChat");
+const table = document.getElementById("table");
 
 socket.emit("room-join", TABLE_ID, "Jag")
