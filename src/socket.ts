c44ce927d8343c031a2cb25a992f031ba4af0ee0
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-06 08:04:16 +0200

    Use the table interface

--- src/socket.ts
+++ src/socket.ts
@@ -1,18 +1,23 @@
 import { Server } from 'socket.io';
+import { Table } from '@interfaces/table.interface';
 
-const tableIds: Record<string, string[]> = {};
+const tableIds: Record<string, Table> = {};
 
 function handleSocketIo_(io: Server) {
   io.on('connect', socket => {
     socket.on('room-join', (tableId, playerName) => {
       socket.join(tableId);
       if (tableId in tableIds) {
-        tableIds[tableId].push(playerName);
+        tableIds[tableId].players.push(playerName);
       } else {
-        tableIds[tableId] = [playerName];
+        tableIds[tableId] = {
+          players: [playerName],
+          pot: 0,
+          activityChat: []
+        };
       }
-      io.sockets.in(tableId).emit('all-players', tableIds[tableId]);
-      io.sockets.in(tableId).emit("new-activity", `${playerName} joined the table`);
+      io.sockets.in(tableId).emit('all-players', tableIds[tableId].players);
+      io.sockets.in(tableId).emit('new-activity', `${playerName} joined the table`);
     });
   });
 }
