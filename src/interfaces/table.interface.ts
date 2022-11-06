800e65ec790268b1c4b7600152b08aca96afb755
JagTheFriend <JagTheFriend12@gmail.com>
2022-11-06 11:45:29 +0200

    No need to store activities

--- src/interfaces/table.interface.ts
+++ src/interfaces/table.interface.ts
@@ -1,10 +1,8 @@
 type Player = string;
 type Pot = number;
-type activityChat = string;
 
 export interface Table {
   players: Player[];
   pot: Pot;
   currentBet: 0;
-  activityChat: activityChat[];
 }
